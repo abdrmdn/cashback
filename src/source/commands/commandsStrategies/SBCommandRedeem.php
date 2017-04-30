9264e75db0600cd6511a15fd3775720519bfa813
Abidul Wahab Ramadan <abidul.rmdn@gmail.com>
2017-04-30 22:25:10 +0800

    finished the redeem command

--- src/source/commands/commandsStrategies/SBCommandRedeem.php
+++ src/source/commands/commandsStrategies/SBCommandRedeem.php
@@ -3,12 +3,36 @@ namespace source\commands\commandsStrategies;
 
 use source\commands\SBCommands;
 use source\commands\SBCommandsInterface;
+use source\util\Domains;
 
-class SBCommandRedeem extends SBCommands implements SBCommandsInterface{
+class SBCommandRedeem extends SBCommands implements SBCommandsInterface
+{
+    private $domainsURIs = [
+        Domains::SINGAPORE => '=> https://' . Domains::SINGAPORE,
+        Domains::MALAYSIA => '=> https://' . Domains::MALAYSIA,
+        Domains::INDONESIA => '=> https://' . Domains::INDONESIA,
+        Domains::TAIWAN => '=> https://' . Domains::TAIWAN,
+        Domains::THAILAND => '=> https://' . Domains::THAILAND,
+        Domains::INTERNATIONAL => '=> https://' . Domains::INTERNATIONAL,
+    ];
 
-
+    /**
+     *
+     */
     public function exec()
     {
-        $this->output('this is RedeemCommand');
+        $domain = $this->parameters[0];
+        $domainUrl = $this->getDomainUrl($domain);
+
+        $this->output('Visit ' . $domainUrl . ' to start earning cashback!');
+    }
+
+    private function getDomainUrl($domain)
+    {
+        if (!isset($this->domainsURIs[$domain])) {
+            throwError('domain provided does not exist');
+        }
+
+        return $this->domainsURIs[$domain];
     }
 }
